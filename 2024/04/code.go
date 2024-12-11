2cad1b0a64af3797ee92927effa4059e348425cb
Manu Phatak <manu@phatak.io>
2024-12-11 17:25:26 -0600

    Solve day 4 part 2

--- 2024/04/code.go
+++ 2024/04/code.go
@@ -10,26 +10,11 @@ func main() {
 	aoc.Harness(run)
 }
 
-func run(part2 bool, input string) any {
-	if part2 {
-		return "not implemented"
-	}
-
-	lines := strings.Split(strings.TrimSpace(input), "\n")
-
-	matches := 0
-	for i, line := range lines {
-		for j := range line {
-			matches += countMatches(lines, i, j)
-		}
-	}
-	return matches
-}
-
 type direction struct {
 	x, y int
 }
 
+// part 1 directions
 var directions = [8]direction{
 	{0, 1},
 	{1, 0},
@@ -41,7 +26,23 @@ var directions = [8]direction{
 	{-1, -1},
 }
 
-func countMatches(lines []string, i, j int) int {
+func run(part2 bool, input string) any {
+	lines := strings.Split(strings.TrimSpace(input), "\n")
+
+	matches := 0
+	for i, line := range lines {
+		for j := range line {
+			if part2 {
+				matches += countMatchesPart2(lines, i, j)
+			} else {
+				matches += countMatchesPart1(lines, i, j)
+			}
+		}
+	}
+	return matches
+}
+
+func countMatchesPart1(lines []string, i, j int) int {
 	count := 0
 	for _, direction := range directions {
 		if lines[i][j] != 'X' {
@@ -76,4 +77,39 @@ func lineInbound(lines []string, i int) bool {
 
 func rowInbound(lines []string, j int) bool {
 	return j >= 0 && j < len(lines[0])
+}
+
+func countMatchesPart2(lines []string, i, j int) int {
+	count := 0
+
+	if (masDR(lines, i, j) || samDR(lines, i, j)) && (masDL(lines, i, j+2) || samDL(lines, i, j+2)) {
+		count += 1
+	}
+
+	return count
+}
+
+func masDR(lines []string, i int, j int) bool {
+	if !lineInbound(lines, i+2) || !rowInbound(lines, j+2) {
+		return false
+	}
+	return lines[i][j] == 'M' && lines[i+1][j+1] == 'A' && lines[i+2][j+2] == 'S'
+}
+func samDR(lines []string, i int, j int) bool {
+	if !lineInbound(lines, i+2) || !rowInbound(lines, j+2) {
+		return false
+	}
+	return lines[i][j] == 'S' && lines[i+1][j+1] == 'A' && lines[i+2][j+2] == 'M'
+}
+func masDL(lines []string, i int, j int) bool {
+	if !lineInbound(lines, i+2) || !rowInbound(lines, j-2) {
+		return false
+	}
+	return lines[i][j] == 'M' && lines[i+1][j-1] == 'A' && lines[i+2][j-2] == 'S'
+}
+func samDL(lines []string, i int, j int) bool {
+	if !lineInbound(lines, i+2) || !rowInbound(lines, j-2) {
+		return false
+	}
+	return lines[i][j] == 'S' && lines[i+1][j-1] == 'A' && lines[i+2][j-2] == 'M'
 }
