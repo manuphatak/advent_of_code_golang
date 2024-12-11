73cf31297343e154f11aab6884fbf5f3f6d48235
Manu Phatak <manu@phatak.io>
2024-12-11 16:13:10 -0600

    Solve day 3 part 2

--- 2024/03/code.go
+++ 2024/03/code.go
@@ -11,29 +11,72 @@ func main() {
 	aoc.Harness(run)
 }
 
-// on code change, run will be executed 4 times:
-// 1. with: false (part1), and example input
-// 2. with: true (part2), and example input
-// 3. with: false (part1), and user input
-// 4. with: true (part2), and user input
-// the return value of each run is printed to stdout
+type Command interface {
+	isCommand()
+}
+type Mul struct {
+	l int
+	r int
+}
+
+func (m Mul) isCommand() {}
+
+type Do struct{}
+
+func (d Do) isCommand() {}
+
+type Dont struct{}
+
+func (d Dont) isCommand() {}
+
 func run(part2 bool, input string) any {
-	// when you're ready to do part 2, remove this "not implemented" block
-	if part2 {
-		return "not implemented"
-	}
 
-	re := regexp.MustCompile(`(?m)(?P<mul>mul\((?P<l>\d{0,3}),(?P<r>\d{0,3})\))`)
-
-	lIndex, rIndex := re.SubexpIndex("l"), re.SubexpIndex("r")
+	commands := parseCommands(input)
 
 	sumProduct := 0
-	for _, match := range re.FindAllStringSubmatch(input, -1) {
-		l, r := parseInt(match[lIndex]), parseInt(match[rIndex])
-		sumProduct += l * r
+
+	if part2 {
+		enabled := true
+		for _, command := range commands {
+			switch c := command.(type) {
+			case Mul:
+				if enabled {
+					sumProduct += c.l * c.r
+				}
+			case Do:
+				enabled = true
+			case Dont:
+				enabled = false
+			}
+		}
+	} else {
+		for _, command := range commands {
+			switch c := command.(type) {
+			case Mul:
+				sumProduct += c.l * c.r
+			}
+		}
 	}
 
 	return sumProduct
+}
+
+func parseCommands(input string) []Command {
+	re := regexp.MustCompile(`(?P<mul>mul\((?P<l>\d{0,3}),(?P<r>\d{0,3})\))|(?P<do>do\(\))|(?P<dont>don't\(\))`)
+
+	mulIndex, lIndex, rIndex, doIndex, dontIndex := re.SubexpIndex("mul"), re.SubexpIndex("l"), re.SubexpIndex("r"), re.SubexpIndex("do"), re.SubexpIndex("dont")
+
+	commands := []Command{}
+	for _, match := range re.FindAllStringSubmatch(input, -1) {
+		if match[mulIndex] != "" {
+			commands = append(commands, Mul{parseInt(match[lIndex]), parseInt(match[rIndex])})
+		} else if match[doIndex] != "" {
+			commands = append(commands, Do{})
+		} else if match[dontIndex] != "" {
+			commands = append(commands, Dont{})
+		}
+	}
+	return commands
 }
 
 func parseInt(s string) int {
