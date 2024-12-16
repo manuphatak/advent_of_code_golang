99c7422d1a5529188f6ac25d547b0f2961270ee6
Manu Phatak <manu@phatak.io>
2024-12-16 13:16:33 -0600

    Solve day 7 part 1

--- 2024/07/code.go
+++ 2024/07/code.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"strconv"
+	"strings"
+
 	"github.com/jpillora/puzzler/harness/aoc"
 )
 
@@ -8,17 +11,59 @@ func main() {
 	aoc.Harness(run)
 }
 
-// on code change, run will be executed 4 times:
-// 1. with: false (part1), and example input
-// 2. with: true (part2), and example input
-// 3. with: false (part1), and user input
-// 4. with: true (part2), and user input
-// the return value of each run is printed to stdout
+type equation struct {
+	test int
+	xs   []int
+}
+
 func run(part2 bool, input string) any {
 	// when you're ready to do part 2, remove this "not implemented" block
 	if part2 {
 		return "not implemented"
 	}
-	// solve part 1 here
-	return 42
+	equations := []equation{}
+
+	for _, line := range strings.Split(strings.TrimSpace(input), "\n") {
+		split := strings.Split(line, ":")
+		equation := equation{parseInt(split[0]), []int{}}
+
+		for _, input := range strings.Split(strings.TrimSpace(split[1]), " ") {
+			equation.xs = append(equation.xs, parseInt(input))
+		}
+
+		equations = append(equations, equation)
+	}
+
+	totalCalibrationResult := 0
+
+	for _, equation := range equations {
+		running, xs := equation.xs[0], equation.xs[1:]
+		if canEqualTest(equation.test, running, xs) {
+			totalCalibrationResult += equation.test
+		}
+	}
+
+	return totalCalibrationResult
+}
+
+func canEqualTest(test, running int, xs []int) bool {
+
+	if running > test {
+		return false
+	}
+
+	if len(xs) == 0 {
+		return running == test
+	}
+
+	x, xs := xs[0], xs[1:]
+	return canEqualTest(test, running+x, xs) || canEqualTest(test, running*x, xs)
+}
+
+func parseInt(s string) int {
+	n, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return n
 }
