4a1c472b1e0381106ed80e671d04c8aa6badf909
Manu Phatak <manu@phatak.io>
2024-12-16 12:38:12 -0600

    Solve day 6 part 2

--- 2024/06/code.go
+++ 2024/06/code.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"maps"
 	"strings"
 
 	"github.com/jpillora/puzzler/harness/aoc"
@@ -51,37 +52,68 @@ func (g guard) Next() point {
 }
 
 type state struct {
-	obstacles  pointSet
-	visited    pointSet
-	guard      guard
-	rows, cols int
+	obstacles     pointSet
+	visited       pointSet
+	loopDetection guardSet
+	guard         guard
+	rows, cols    int
 }
 
 func newState(rows int, cols int) *state {
 	return &state{
-		obstacles: pointSet{},
-		visited:   pointSet{},
-		guard:     guard{},
-		rows:      rows,
-		cols:      cols,
+		obstacles:     pointSet{},
+		visited:       pointSet{},
+		loopDetection: guardSet{},
+		guard:         guard{},
+		rows:          rows,
+		cols:          cols,
 	}
+}
+
+func (s *state) Run() (int, bool) {
+	for {
+		if s.guardIsOnMap() {
+			s.markPositionVisited()
+		} else {
+			break
+		}
+
+		if s.obstacleInNext() {
+			ok := s.turnRight()
+
+			if !ok {
+				return len(s.visited), false
+			}
+		} else {
+			s.moveForward()
+		}
+	}
+
+	return len(s.visited), true
 }
 
 func (s *state) SetGuard(guard guard) {
 	s.guard = guard
 }
+func (s state) Clone() *state {
+	nextState := newState(s.rows, s.cols)
+	nextState.obstacles = maps.Clone(s.obstacles)
+	nextState.SetGuard(s.guard)
 
-func (s state) GuardIsOnMap() bool {
+	return nextState
+}
+
+func (s state) guardIsOnMap() bool {
 	return s.guard.IsOnMap(s.cols, s.rows)
 }
-func (s *state) MarkPositionVisited() {
+func (s *state) markPositionVisited() {
 	s.visited.Add(s.guard.point)
 
 }
-func (s state) ObstacleInNext() bool {
+func (s state) obstacleInNext() bool {
 	return s.obstacles.Has(s.guard.Next())
 }
-func (s *state) TurnRight() {
+func (s *state) turnRight() bool {
 	switch s.guard.dir {
 	case UP:
 		s.guard.dir = RIGHT
@@ -93,8 +125,15 @@ func (s *state) TurnRight() {
 		s.guard.dir = UP
 	}
 
+	if s.loopDetection.Has(s.guard) {
+		return false
+	} else {
+		s.loopDetection.Add(s.guard)
+		return true
+	}
+
 }
-func (s *state) MoveForward() {
+func (s *state) moveForward() {
 	s.guard.point = s.guard.Next()
 }
 
@@ -108,11 +147,17 @@ func (s pointSet) Has(p point) bool {
 	return ok
 }
 
+type guardSet map[guard]struct{}
+
+func (s guardSet) Add(g guard) {
+	s[g] = struct{}{}
+}
+func (s guardSet) Has(g guard) bool {
+	_, ok := s[g]
+	return ok
+}
+
 func run(part2 bool, input string) any {
-	// when you're ready to do part 2, remove this "not implemented" block
-	if part2 {
-		return "not implemented"
-	}
 
 	// 	read in the map, find the guard position
 	lines := strings.Split(strings.TrimSpace(input), "\n")
@@ -131,19 +176,40 @@ func run(part2 bool, input string) any {
 		}
 	}
 
-	for {
-		if state.GuardIsOnMap() {
-			state.MarkPositionVisited()
-		} else {
-			break
+	if part2 {
+		count := 0
+		for i := 0; i < state.rows; i++ {
+			for j := 0; j < state.cols; j++ {
+				// for each point...
+				point := point{j, i}
+
+				// ...if it's an obstacle, skip it
+				if state.obstacles.Has(point) {
+					continue
+				}
+
+				// ...otherwise, clone the state, add the obstacle, and run the simulation
+				newState := state.Clone()
+				newState.obstacles.Add(point)
+				_, ok := newState.Run()
+
+				// if the simulation failed (found an infinite loop), increment the count
+				if !ok {
+					count += 1
+				}
+
+			}
 		}
 
-		if state.ObstacleInNext() {
-			state.TurnRight()
-		} else {
-			state.MoveForward()
+		return count
+
+	} else {
+		state.obstacles.Add(point{0, 0})
+		visited, ok := state.Run()
+
+		if !ok {
+			panic("loop detected in part 1")
 		}
+		return visited
 	}
-
-	return len(state.visited)
 }
