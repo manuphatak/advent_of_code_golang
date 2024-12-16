730c8e95602bdb0571514fe74c407ddc3767b508
Manu Phatak <manu@phatak.io>
2024-12-16 11:45:53 -0600

    Solve day 6 part 1

--- 2024/06/code.go
+++ 2024/06/code.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"strings"
+
 	"github.com/jpillora/puzzler/harness/aoc"
 )
 
@@ -8,17 +10,140 @@ func main() {
 	aoc.Harness(run)
 }
 
-// on code change, run will be executed 4 times:
-// 1. with: false (part1), and example input
-// 2. with: true (part2), and example input
-// 3. with: false (part1), and user input
-// 4. with: true (part2), and user input
-// the return value of each run is printed to stdout
+type direction int
+
+const (
+	UP direction = iota
+	RIGHT
+	DOWN
+	LEFT
+)
+
+type point struct {
+	x, y int
+}
+
+type guard struct {
+	point point
+	dir   direction
+}
+
+func (g guard) IsOnMap(cols, rows int) bool {
+	return g.point.x >= 0 && g.point.x < cols && g.point.y >= 0 && g.point.y < rows
+
+}
+
+func (g guard) Next() point {
+
+	switch g.dir {
+	case UP:
+		return point{g.point.x, g.point.y - 1}
+	case RIGHT:
+		return point{g.point.x + 1, g.point.y}
+	case DOWN:
+		return point{g.point.x, g.point.y + 1}
+	case LEFT:
+		return point{g.point.x - 1, g.point.y}
+
+	}
+
+	panic("unknown direction")
+}
+
+type state struct {
+	obstacles  pointSet
+	visited    pointSet
+	guard      guard
+	rows, cols int
+}
+
+func newState(rows int, cols int) *state {
+	return &state{
+		obstacles: pointSet{},
+		visited:   pointSet{},
+		guard:     guard{},
+		rows:      rows,
+		cols:      cols,
+	}
+}
+
+func (s *state) SetGuard(guard guard) {
+	s.guard = guard
+}
+
+func (s state) GuardIsOnMap() bool {
+	return s.guard.IsOnMap(s.cols, s.rows)
+}
+func (s *state) MarkPositionVisited() {
+	s.visited.Add(s.guard.point)
+
+}
+func (s state) ObstacleInNext() bool {
+	return s.obstacles.Has(s.guard.Next())
+}
+func (s *state) TurnRight() {
+	switch s.guard.dir {
+	case UP:
+		s.guard.dir = RIGHT
+	case RIGHT:
+		s.guard.dir = DOWN
+	case DOWN:
+		s.guard.dir = LEFT
+	case LEFT:
+		s.guard.dir = UP
+	}
+
+}
+func (s *state) MoveForward() {
+	s.guard.point = s.guard.Next()
+}
+
+type pointSet map[point]struct{}
+
+func (s pointSet) Add(p point) {
+	s[p] = struct{}{}
+}
+func (s pointSet) Has(p point) bool {
+	_, ok := s[p]
+	return ok
+}
+
 func run(part2 bool, input string) any {
 	// when you're ready to do part 2, remove this "not implemented" block
 	if part2 {
 		return "not implemented"
 	}
-	// solve part 1 here
-	return 42
+
+	// 	read in the map, find the guard position
+	lines := strings.Split(strings.TrimSpace(input), "\n")
+
+	state := newState(len(lines), len(lines[0]))
+
+	for i, line := range lines {
+		for j, cell := range line {
+			if cell == '#' {
+				state.obstacles.Add(point{j, i})
+			}
+			if cell == '^' {
+				state.SetGuard(guard{point{j, i}, UP})
+
+			}
+		}
+	}
+
+	for {
+		if state.GuardIsOnMap() {
+			state.MarkPositionVisited()
+		} else {
+			break
+		}
+
+		if state.ObstacleInNext() {
+			state.TurnRight()
+		} else {
+			state.MoveForward()
+		}
+	}
+
+	return len(state.visited)
 }
